319da110fe2f10a9ce1618e6d337a5d3b6867d4c
Bean, Andrew K <andan@bsg.ac.uk>
2024-01-25 15:55:51 +0000

    fix: export function

--- R/send_data.R
+++ R/send_data.R
@@ -1,4 +1,4 @@
-
+#' @export
 send_data <- function() {
   sendmailR::sendmail_options(smtpServer=Sys.getenv("SMTP_SERVER"),verbose=TRUE)
   sendmailR::sendmail('noreply@micronutrient.support', 'github@micronutrient.support', 'Test', msg=list(sendmailR::mime_part("Here it is")))
