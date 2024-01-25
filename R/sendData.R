620067b057c69528cf507eaaba5333bbda7409aa
Bean, Andrew K <andan@bsg.ac.uk>
2024-01-25 16:06:40 +0000

    docs: add docs

--- R/sendData.R
+++ R/sendData.R
@@ -1,5 +1,16 @@
+#' @title Send Data
+#'
+#' @description
+#' Sends data via email
+#'
+#' @details
+#' This function can be used to do some things
+#'
+#' @return output
+#'
 #' @export
-send_data <- function() {
+
+sendData <- function() {
   sendmailR::sendmail_options(smtpServer=Sys.getenv("SMTP_SERVER"),verbose=TRUE)
   sendmailR::sendmail('noreply@micronutrient.support', 'github@micronutrient.support', 'Test', msg=list(sendmailR::mime_part("Here it is")))
 }
