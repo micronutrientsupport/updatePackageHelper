238a6f3863074259ad90c744a90871e8fcc32ac3
Bean, Andrew K <andan@bsg.ac.uk>
2024-01-26 14:49:33 +0000

    chore: debug

--- R/sendData.R
+++ R/sendData.R
@@ -13,23 +13,23 @@
 #' @export
 
 sendData <- function(dataframes_list) {
-  sendmailR::sendmail_options(smtpServer=Sys.getenv("SMTP_SERVER"),verbose=TRUE)
-    # Save named dataframes as .rda files
-    for (name in names(dataframes_list)) {
-      saveRDS(dataframes_list[[name]], file = paste0(name, ".rda"))
-    }
-
-    # Attach .rda files to the email
-    attachments <- list.files(pattern = "\\.rda$")
-
-    # Compose and send the email
-    sendmailR::sendmail(from = "noreply@micronutrient.support",
-             to = "github@micronutrient.support",
-             subject = "File",
-             body = "Here it is",
-             attach.files = attachments)
-
-    # Clean up: remove temporary .rda files
+  # sendmailR::sendmail_options(smtpServer=Sys.getenv("SMTP_SERVER"),verbose=TRUE)
+  #   # Save named dataframes as .rda files
+  #   for (name in names(dataframes_list)) {
+  #     saveRDS(dataframes_list[[name]], file = paste0(name, ".rda"))
+  #   }
+  #
+  #   # Attach .rda files to the email
+  #   attachments <- list.files(pattern = "\\.rda$")
+  #
+  #   # Compose and send the email
+  #   sendmailR::sendmail(from = "noreply@micronutrient.support",
+  #            to = "github@micronutrient.support",
+  #            subject = "File",
+  #            body = "Here it is",
+  #            attach.files = attachments)
+  #
+  #   # Clean up: remove temporary .rda files
     file.remove(attachments)
 
     res <- list("Hello")
