75667ae0ff603f76fd6ded145dd803a9a5a69a6e
Andrew Bean <39411049+bgsandan@users.noreply.github.com>
2024-01-26 15:48:55 +0000

    Update sendData.R

--- R/sendData.R
+++ R/sendData.R
@@ -14,26 +14,30 @@
 
 sendData <- function(dataframeList) {
   sendmailR::sendmail_options(smtpServer=Sys.getenv("SMTP_SERVER"),verbose=TRUE)
-    # Save named dataframes as .rda files
-    for (name in names(dataframeList)) {
-      saveRDS(dataframeList[[name]], file = paste0(name, ".rda"))
-    }
 
-    # Attach .rda files to the email
-    attachments <- list.files(pattern = "\\.rda$")
+  # Save named dataframes as .rda files
+  for (name in names(dataframeList)) {
+    df <- dataframeList[[name]]
+    save(df, file=paste0(name, ".rda"))
+  }
+  
+  # Attach .rda files to the email
+  attachments <- list.files(pattern = "\\.rda$")
+  
+  mime <- list(sendmailR::mime_part("hello"))
+  for (a in attachments) {
+    mime <- append(mime, sendmailR::mime_part(a))
+  }
+  
+  
+  sendmailR::sendmail(
+    'noreply@bgs.ac.uk', 
+    'github@micronutrient.support', 
+    'Test', 
+    msg=mime)
+  
+  file.remove(attachments)
 
-    # Compose and send the email
-    sendmailR::sendmail(from = "noreply@micronutrient.support",
-             to = "github@micronutrient.support",
-             subject = "File",
-             body = "Here it is",
-             attach.files = attachments)
-
-    # Clean up: remove temporary .rda files
-    file.remove(attachments)
-
-    res <- list("Hello")
-
-    return(res)
+  
+  return(attachments)
 }
-
