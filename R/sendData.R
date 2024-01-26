5ee4e6ab797997732ede2494d7ff2d6120367912
Bean, Andrew K <andan@bsg.ac.uk>
2024-01-26 12:15:34 +0000

    feat: add emailing of dataframes

--- R/sendData.R
+++ R/sendData.R
@@ -6,11 +6,30 @@
 #' @details
 #' This function can be used to do some things
 #'
+#' @param dataframes_list list of data to send
+#'
 #' @return output
 #'
 #' @export
 
-sendData <- function() {
+sendData <- function(dataframes_list) {
   sendmailR::sendmail_options(smtpServer=Sys.getenv("SMTP_SERVER"),verbose=TRUE)
-  sendmailR::sendmail('noreply@micronutrient.support', 'github@micronutrient.support', 'Test', msg=list(sendmailR::mime_part("Here it is")))
+    # Save named dataframes as .rda files
+    for (name in names(dataframes_list)) {
+      saveRDS(dataframes_list[[name]], file = paste0(name, ".rda"))
+    }
+
+    # Attach .rda files to the email
+    attachments <- list.files(pattern = "\\.rda$")
+
+    # Compose and send the email
+    sendmailR::sendmail(from = "noreply@micronutrient.support",
+             to = "github@micronutrient.support",
+             subject = "File",
+             body = "Here it is",
+             attach.files = attachments)
+
+    # Clean up: remove temporary .rda files
+    file.remove(attachments)
 }
+
