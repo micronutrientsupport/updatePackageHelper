fad07af2cc8010d462a5edc211072188a7dd0e81
Bean, Andrew K <andan@bsg.ac.uk>
2024-05-14 08:23:05 +0100

    feat: basic functions to install and update R packages

---
+++ R/updatePackage.R
@@ -0,0 +1,18 @@
+#' @title Triggers a re-install of the packahge URL to ensure latest data
+#'
+#' @description
+#' Update Package
+#'
+#' @details
+#' This function can be used to do some things
+#'
+#' @param dataframes_list list of data to send
+#'
+#' @return output
+#'
+#' @export
+
+updatePackage <- function() {
+  install.packages(Sys.getenv("PACKAGE_URL"), repos=NULL, type="source")
+  return(true)
+}
